96ba2854be7a0387b1db77a9f3a6e453b4b98eaa
Sophin Shrestha <Sophin.Shrestha@pacifichunter.com.au>
2022-03-09 15:52:08 +1100

    Setup Font

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,17 +1,64 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 
 import Ionicon from "react-native-vector-icons/Ionicons";
 
+import useFonts from "../hooks/useFonts";
+import AppLoading from "expo-app-loading";
+
 export default function HomeScreen() {
+  const [isReady, setIsReady] = React.useState(false);
+
+  const LoadFonts = async () => {
+    await useFonts();
+  };
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={LoadFonts}
+        onFinish={() => setIsReady(true)}
+        onError={() => {
+          console.log(console.warn);
+        }}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <View style={styles.topIcon}>
-        <Ionicon name="menu-outline" size={30} color="white" />
-        <Ionicon name="person-circle-outline" size={30} color="white" />
+        <Ionicon name="menu-outline" size={40} color="white" />
+        <Ionicon name="person-circle-outline" size={40} color="white" />
       </View>
-      <Text>HomeScreen</Text>
-      <Button title="Go to Details" />
+      <View
+        style={{
+          paddingHorizontal: 30,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: "#d4d4d4",
+            fontSize: 30,
+            fontFamily: "PoppinsThin",
+          }}
+        >
+          New collection
+        </Text>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 40,
+            fontWeight: "bold",
+            fontFamily: "PoppinsSemiBold",
+          }}
+        >
+          with
+          <Text style={{ color: "#a8f069" }}> 15% </Text>
+          <Text style={styles.text}>discount</Text>
+        </Text>
+      </View>
     </View>
   );
 }
@@ -20,7 +67,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignContent: "center",
-    backgroundColor: `rgba(59, 87, 69, 0.7)`,
+    backgroundColor: `rgba(25, 26, 25, 0.7)`,
   },
   topIcon: {
     marginTop: 60,
---
+++ hooks/useFonts.js
@@ -0,0 +1,7 @@
+import * as Font from "expo-font";
+
+export default useFonts = async () =>
+  await Font.loadAsync({
+    PoppinsThin: require("../assets/fonts/Poppins-Thin.ttf"),
+    PoppinsSemiBold: require("../assets/fonts/Poppins-SemiBold.ttf"),
+  });
