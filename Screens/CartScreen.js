76ee0c80744c96fff11ac332e807276fb722beb7
SOPHIN SHRESTHA <sophinstha123@gmail.com>
2022-05-16 18:03:56 +1000

    Added CartScreen and SearchScreen Notice

--- Screens/CartScreen.js
+++ Screens/CartScreen.js
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import LottieView from "lottie-react-native";
+import useFonts from "../hooks/useFonts";
 
 export default function CartScreen() {
+  const animation = useRef(null);
+  useEffect(() => {
+    // animation.current?.play();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>CartScreen</Text>
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          justifyContent: "center",
+          marginTop: 20,
+        }}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 30,
+            fontFamily: "PoppinsSemiBold",
+          }}
+        >
+          Working On It
+        </Text>
+      </View>
+      <View style={styles.animationContainer}>
+        <LottieView
+          autoPlay
+          ref={animation}
+          style={{
+            width: "100%",
+            height: "90%",
+          }}
+          // Find more Lottie files at https://lottiefiles.com/featured
+          source={require("../assets/lottiePackage/shoppingCart.json")}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
+  animationContainer: {
+    backgroundColor: "#fff",
     alignItems: "center",
-    backgroundColor: `rgba(59, 87, 69, 0.7)`,
+    justifyContent: "center",
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
   },
 });
--- Screens/SearchScreen.js
+++ Screens/SearchScreen.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import LottieView from "lottie-react-native";
 
 export default function SearchScreen() {
+  const animation = useRef(null);
+  useEffect(() => {
+    // animation.current?.play();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>SearchScreen</Text>
+    <View style={styles.animationContainer}>
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          marginLeft: 40,
+          width: "100%",
+          height: "90%",
+        }}
+        // Find more Lottie files at https://lottiefiles.com/featured
+        source={require("../assets/lottiePackage/comingSoon.json")}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
+  animationContainer: {
+    backgroundColor: "#fff",
     alignItems: "center",
-    backgroundColor: `rgba(59, 87, 69, 0.7)`,
+    justifyContent: "center",
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
   },
 });
